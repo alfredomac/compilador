1b37cce48a822a3efba33c2f90f138d2c703ed5b
another <alfredomac2001@gmail.com>
2024-09-02 21:21:12 -0300

    Reconhecimento de Identificador

--- compilador.c
+++ compilador.c
@@ -3,7 +3,7 @@
 Programa: Compilador Macutunda
 Autor: Alfredo Macutunda
 Data de Início: 24/08/2024
-Última modificação: 27/08/2024
+Última modificação: 02/09/2024
 */
 
 #include <stdio.h>
@@ -17,18 +17,18 @@ char buffers[MAX_LINHA];
 char lexema[MAX_LINHA];
 unsigned short tipo = 0;
 int linhAtual=0,colunaAtual=0;
+int aux=0;
 unsigned short estado,fim=0;
 
 
 int abrirArquivo (char []);
 void lerArquivo ();
 unsigned short delimitadorLexema(unsigned short);
-unsigned short automato(unsigned short); 
-void analisadorLexico();
+unsigned short analisadorLexico(unsigned short); 
 
 void limparString (char *cadeia){
     for(unsigned short i=0;i<strlen(lexema);i++){
-        cadeia[i]=0;
+        cadeia[i]=' ';
     }
 }
 
@@ -57,7 +57,7 @@ int abrirArquivo( char nomeArquivo[] ){
     }
 }
 
-unsigned short automato(unsigned short Ccaracter){
+unsigned short analisadorLexico(unsigned short Ccaracter){
     
     /* Essa função tem os seguintes retornos:
         1 - Continua  
@@ -69,62 +69,80 @@ unsigned short automato(unsigned short Ccaracter){
     unsigned short int  c = 0 ; // Variável controlada ( 1 - Tudo bem ; 0 - Erro) ;
 
     if (estado==0){ // Determinar tipo de autômato. O 1º caracter quem determina o tipo é o caracter
-        if( (Ccaracter>=48) && (Ccaracter<=57) ){ // Se for um número //Número
+        switch (Ccaracter){
+        case 48 ... 57 :
             c = 1;
             tipo = 1;
-        } else{
-
+        break;
+        case 65 ... 90: // Letra maiuscula
+        case 97 ... 122: // Letra minuscula
+        case 95: // Undercore
+            c=1;
+            tipo=3;
+        break;
         }
+
         if(c==1){
             estado++; // Passa para para o próximo estado
         }
     } else {
         switch (tipo) { // A partir do 2º é determinado pelo caracter anterior
         case 1: 
-        case 2:
-        //Número
+        case 2: //Número
             switch (estado){ // Estado de transição
             case 1:
-                if( (Ccaracter>=48) && (Ccaracter<=57) ){ // Se for um número
+                switch (Ccaracter){
+                case 48 ... 57 :
                     c = 1;
                     fim = 1;
-                }
-                if(Ccaracter==46){// Se for ponto
+                break;
+                case 46:
                     c=1;
                     estado++;
                     tipo = 2; //Número real
-                } 
                 break;
+                }
+            break;
             case 2:
                 fim = 0;
-                if( (Ccaracter>=48) && (Ccaracter<=57) ){ // Se for um número
+                switch (Ccaracter){
+                case 48 ... 57 : // Se for um número
                     c = 1;
                     estado ++;
-                } 
                 break;
+                }
+            break;
             case 3:
-                fim = 1;
-                if( (Ccaracter>=48) && (Ccaracter<=57) ){ // Se for um número
+               fim = 1;
+               switch (Ccaracter){
+               case 48 ... 57 : // Se for um número
                     c = 1;
-                } 
-                break;
-            default:
-                break;
+               break;
+               }
+            break;
             }
             break;
-        
-        default:
+        case 3: //Identificador
+            fim = 1;
+            switch (Ccaracter){
+            case 48 ... 57 : // Se for um número
+            case 65 ... 90: // Se for letra maiusculas
+            case 97 ... 122: // Se for letra minuscula
+            case 95: // Undercore
+                c=1;
             break;
+            }
+        break;
         }
     }
-
+    //printf("%s - %d\n", lexema,tipo);
     if(c==1){
-        lexema[colunaAtual] = caracter;  
+        lexema[aux] = caracter;  
+        //printf("%s - %d\n", lexema,tipo);
         return 1;
     }else{
-        if((delimitadorLexema(Ccaracter)==1) && (fim==1)){
-            printf("%s - %d\n", lexema,tipo);
-            limparString(lexema);
+        if((delimitadorLexema(Ccaracter)==1) && (fim==1)){ //Terminar com sucesso
+            //printf("%s - %d\n", lexema,tipo);
             return 2;
         }else{
             return 0;
@@ -134,7 +152,6 @@ unsigned short automato(unsigned short Ccaracter){
 
 void lerArquivo (){
     int pos, nLinha;
-    unsigned short int tipo = 0;
     char caracter;
     unsigned short Ccaracter;
     linhAtual = 1;
@@ -143,21 +160,29 @@ void lerArquivo (){
             fgets(buffers,MAX_LINHA,arq);
             colunaAtual=0;
             estado=0;
+            //printf("Lexema: %s  - %d \n",buffers,strlen(buffers) -1);
             while(colunaAtual<=strlen(buffers) -1 ){
                 caracter = buffers[colunaAtual];
                 Ccaracter = caracter;
-                switch (automato(Ccaracter))  {
+                //printf("%d Caracter : %d\n", colunaAtual+1, Ccaracter );
+                switch (analisadorLexico(Ccaracter))  {
                 case 2:
-                    printf("Lexema: %s",lexema);
+                    printf("Lexema: %s - %d\n",lexema,tipo);
                     limparString(lexema);
+                    aux=0;
+                    estado=0;
                     break;
                 case 0:
-                    printf("Erro na linha %d coluna %d \n", linhAtual, (colunaAtual - strlen(lexema) +1 ));    
+                    printf("Erro na linha %d coluna %d \n", linhAtual, (colunaAtual - strlen(lexema) +1 ));   
+                    colunaAtual = strlen(buffers); 
+                    limparString(lexema);
+                    estado=0;
                     break;
                 default:
                     break;
                 }
                 colunaAtual++;
+                aux++;
             }
             linhAtual++;
         //}        
